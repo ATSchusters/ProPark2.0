d006c3f55a4edf6426a39e4476e7043a45a30ef9
Nathan Hartzell <nhartzel@uncc.edu>
2022-11-17 21:10:49 -0500

    import database models on main

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+import os
+
+import bcrypt
+from flask import Flask, redirect, url_for, render_template, request
+from database import db
+from models import User as User
+from models import Event as Event
+from models import Deck as Deck
+
+
 
 app = Flask(__name__)
 
